569cfb9c6be34ee0f5e1c691c799bbaabf5d0f6b
Tiggum <caden.james.reynolds@gmail.com>
2020-11-10 18:37:49 +0100

    finished challenge

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ app.use(bodyParser.json())
 const students = [
     {
         name: 'caden',
-        grades: 90,
+        grades: {
+            math: 90,
+            english: 84,
+            bio: 12
+        },
         id: 0
     },
     {
         name: 'rohn',
-        grades: 91,
+        grades: {
+            math: 85,
+            english: 90,
+            crafts: 100
+        },
         id: 1
     }
 ]
@@ -32,6 +40,35 @@ app.get('/students', (req, res) => {
 app.get('/students/:id', (req, res) => {
     let id = parseInt(req.params.id)
     res.send(students.find(student => id === student.id))
+})
+
+app.get('/grades/:id', (req, res) => {
+    let id = parseInt(req.params.id)
+    let student = students.find(student => id === student.id)
+    res.send(student.grades)
+})
+
+app.post('/grades/', (req, res) => {
+    let id = parseInt(req.body.id)
+    let grade = req.body.grade
+    console.log(id)
+    let student = students.find(student => id === student.id)
+    console.log(student)
+    if (student && grade) {
+        res.send("Grade successfully added")
+    } else {
+        res.send("Student not found")
+    }
+})
+
+app.post('/register/', (req, res) => {
+    let email = req.body.email
+    let username = req.body.username
+    if (email && username) {
+        res.send(`Added ${username} with ${email}`)
+    } else {
+        res.send("malformed register")
+    }
 })
 
 
